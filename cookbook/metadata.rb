21c41c67d25450002980289418412efe54393179
jmanero <john.manero@gmail.com>
2015-02-05 01:17:49 -0500

    Fix path resolution in cookbook metadata

--- cookbook/metadata.rb
+++ cookbook/metadata.rb
@@ -3,8 +3,8 @@ maintainer 'John Manero'
 maintainer_email 'john.manero@gmail.com'
 description 'Berkshelf API for S3'
 
-license IO.read('../LICENSE') rescue 'MIT'
-long_description IO.read('../README.md') rescue ''
-version IO.read('../VERSION') rescue '0.0.1'
+license IO.read(File.absolute_path('../../LICENSE', __FILE__)) rescue 'MIT'
+long_description IO.read(File.absolute_path('../../README.md', __FILE__)) rescue ''
+version IO.read(File.absolute_path('../../VERSION', __FILE__)) rescue '0.0.1'
 
 depends 'apt'
